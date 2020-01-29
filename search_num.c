179666ca6e92f751cbb55e80adaeba6ed975ddbd
wodlxosxos <43673102+wodlxosxos@users.noreply.github.com>
2020-01-29 18:16:01 +0900

    search_num.c commit
    찾는 element의 갯수 출력

---
+++ search_num.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num_of_input, num_of_search;
+    scanf("%d %d", &num_of_input, &num_of_search);
+    int *input_num = (int *)malloc(sizeof(int) * num_of_input);
+    int *search_num = (int *)malloc(sizeof(int) * num_of_search);
+    int *num_check = (int *)malloc(sizeof(int) * num_of_search);
+    for (int i = 0; i < num_of_input; i++)
+        scanf("%d", &input_num[i]);
+    for (int i = 0; i < num_of_search; i++)
+    {
+        scanf("%d", &search_num[i]);
+        num_check[i] = 0;
+    }
+    for (int i = 0; i < num_of_input; i++)
+    {
+        for (int j = 0; j < num_of_search; j++)
+        {
+            if (input_num[i] == search_num[j])
+            {
+                num_check[j]++;
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < num_of_search; i++)
+        printf("%d\n", num_check[i]);
+}
