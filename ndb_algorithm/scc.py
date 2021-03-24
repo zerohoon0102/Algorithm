b206f14a6f56b20a25ce61a56413ab5745f93f4e
윤영훈 <wodlxosxos73@gmail.com>
2021-03-24 21:09:49 +0900

    Update scc.py

--- ndb_algorithm/scc.py
+++ ndb_algorithm/scc.py
@@ -1,22 +1,28 @@
 MAX = 10001
 
+def double_arr(n, a):
+    if a == None:
+        return [[] for __ in range(n)]
+    return [[a for _ in range(n)] for __ in range(n)]
+
 d = [0]*MAX
 int_id = 0
 finished = [False]*MAX
-a = [[]]*MAX
+a = double_arr(MAX, None)
 
 SCC = []
 stack = []
 
-def dfs(n):
-    d[n] = ++int_id
+def dfs(n, int_id):
+    int_id += 1
+    d[n] = int_id
     stack.append(n)
 
     parent = d[n]
-    for i in range(len(a)):
+    for i in range(len(a[n])):
         y = a[n][i]
         if d[y] == 0:
-            parent = min(parent, dfs(y))
+            parent = min(parent, dfs(y, int_id))
         elif finished[y] == False:
             parent = min(parent, d[y])
 
@@ -49,5 +55,5 @@ if __name__=="__main__":
     a[11].append(8)
     for i in range(1, v+1):
         if(d[i] == 0):
-            dfs(i)
+            dfs(i, int_id)
     print(SCC)
