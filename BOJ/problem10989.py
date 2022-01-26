3ca50d1e373e575b633edc807152185f3c214945
윤영훈 <wodlxosxos73@gmail.com>
2022-01-26 13:56:06 +0900

    solve BJ 10989

--- BOJ/problem10989.py
+++ BOJ/problem10989.py
@@ -1,11 +1,13 @@
 import sys
 
-N = int(sys.stdin.readline())
-n_arr = [0,]*10001
+N = int(sys.stdin.readline().rstrip())
 
+result = [0,]*10000
 for _ in range(N):
-    n_arr[int(sys.stdin.readline())] += 1
+    result[int(sys.stdin.readline().rstrip())-1] += 1
 
-for i in range(10001):
-    if n_arr[i] != 0:
-        print(str(i) + ("\n" + str(i)) * (n_arr[i] - 1))
+for i in range(10000):
+    for _ in range(result[i]):
+        print(i+1)
+
+
