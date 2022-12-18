e9dad476915189407ddd1c9407e20751609ef267
zerohoon0102 <wodlxosxos73@gmail.com>
2022-12-18 22:20:55 +0900

    PROG : 118668

--- programmers/coding118668.py
+++ programmers/coding118668.py
@@ -1,35 +1,34 @@
 from collections import deque
 
 def solution(alp, cop, problems):
-    answer = 100000
     problems.append([0,0,0,1,1])
     problems.append([0,0,1,0,1])
     alp_problems = sorted(problems, key=lambda x: (-(x[2]/x[4]), x[4]))
     cop_problems = sorted(problems, key=lambda x: (-(x[3]/x[4]), x[4]))
     
-    dp = [[100000000]*301 for _ in range(301)]
-    dp[alp][cop] = 0
     target = [alp,cop]
     for problem in problems:
         target[0] = max(target[0], problem[0])
         target[1] = max(target[1], problem[1])
+    dp = [[100000000]*(target[1]+1) for _ in range(target[0]+1)]
+    dp[alp][cop] = 0
     
     queue = deque([(alp,cop)])
     while queue:
         alp, cop = queue.popleft()
         cost = dp[alp][cop]
         if alp >= target[0] and cop >= target[1]:
-            answer = min(answer, cost)
             continue
-        if alp < target[0] and cop < target[1]:
+        elif alp < target[0] and cop < target[1]:
             for problem in problems:
                 if alp >= problem[0] and cop >= problem[1] :
                     nxt_cost = cost+problem[4]
                     nxt_alp = alp+problem[2]
                     nxt_cop = cop+problem[3]
                     
-                    nxt_alp = min(nxt_alp, 300)
-                    nxt_cop = min(nxt_cop, 300)
+                    nxt_alp = min(nxt_alp, target[0])
+                    nxt_cop = min(nxt_cop, target[1])
+                    
                     if dp[nxt_alp][nxt_cop] > nxt_cost:
                         dp[nxt_alp][nxt_cop] = nxt_cost
                         queue.append((nxt_alp, nxt_cop))
@@ -38,24 +37,23 @@ def solution(alp, cop, problems):
                 if alp >= problem[0] and cop >= problem[1]:
                     nxt_cost = cost+problem[4]
                     nxt_alp = alp+problem[2]
-                    nxt_cop = cop+problem[3]
-                    break
-            nxt_alp = min(300, nxt_alp)
-            nxt_cop = min(300, nxt_cop)
-            if dp[nxt_alp][nxt_cop] > nxt_cost:
-                dp[nxt_alp][nxt_cop] = nxt_cost
-                queue.append((nxt_alp, nxt_cop))
+                    if nxt_alp >= target[0]:
+                        nxt_alp = target[0]
+                        chk = True
+                    if dp[nxt_alp][target[1]] > nxt_cost:
+                        dp[nxt_alp][target[1]] = nxt_cost
+                        queue.append((nxt_alp, target[1]))
         elif alp >= target[0] and cop < target[1]:
             for problem in cop_problems:
                 if alp >= problem[0] and cop >= problem[1]:
                     nxt_cost = cost+problem[4]
-                    nxt_alp = alp+problem[2]
                     nxt_cop = cop+problem[3]
-                    break
-            nxt_alp = min(300, nxt_alp)
-            nxt_cop = min(300, nxt_cop)
-            if dp[nxt_alp][nxt_cop] > nxt_cost:
-                dp[nxt_alp][nxt_cop] = nxt_cost
-                queue.append((nxt_alp, nxt_cop))
+                    if nxt_cop >= target[1]:
+                        nxt_cop = target[1]
+                        chk = True
+                    
+                    if dp[target[0]][nxt_cop] > nxt_cost:
+                        dp[target[0]][nxt_cop] = nxt_cost
+                        queue.append((target[0], nxt_cop))
     
-    return answer
+    return dp[-1][-1]
