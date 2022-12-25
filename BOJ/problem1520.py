7a5cb8ae68dfc3426728db1a5b360ae93eb0ce43
zerohoon0102 <wodlxosxos73@gmail.com>
2022-12-26 01:15:49 +0900

    BOJ : 1520 v2

--- BOJ/problem1520.py
+++ BOJ/problem1520.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 
 M, N = map(int, input().rstrip().split())
 table = [list(map(int, input().rstrip().split())) for _ in range(M)]
-chk = [[[-1]*4 for _ in range(N)] for _ in range(M)]
+chk = [[-1]*N for _ in range(M)]
 ver = [-1, 1, 0, 0]
 hor = [0, 0, -1, 1]
 
@@ -16,17 +16,17 @@ def dfs(i, j):
     if i == M-1 and j == N-1:
         return 1
     cur_result = 0
-    for idx, v in enumerate(chk[i][j]):
-        if v != -1:
-            cur_result += v
-        else:
-            nxt_i = i+ver[idx]
-            nxt_j = j+hor[idx]
-            if (0 <= nxt_i <= M-1 and 0 <= nxt_j <= N-1 and
-                table[nxt_i][nxt_j] < table[i][j]):
+    for idx in range(4):
+        nxt_i = i+ver[idx]
+        nxt_j = j+hor[idx]
+        if (0 <= nxt_i <= M-1 and 0 <= nxt_j <= N-1 and
+            table[nxt_i][nxt_j] < table[i][j]):
+            if chk[nxt_i][nxt_j] == -1:
                 cur = dfs(nxt_i, nxt_j)
-                chk[i][j][idx] = cur
+                chk[nxt_i][nxt_j] = cur
                 cur_result += cur
+            else:
+                cur_result += chk[nxt_i][nxt_j]
     
     return cur_result
 
