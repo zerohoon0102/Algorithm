8fc146af500b297270f040c0a964acac1b5035a5
zerohoon0102 <wodlxosxos73@gmail.com>
2023-01-17 01:24:49 +0900

    PROG : 92343

---
+++ programmers/problem92343.py
@@ -0,0 +1,46 @@
+from collections import deque
+def solution(info, edges):
+    answer = 0
+    tree = [[-1, -1] for _ in range(len(info))]
+    for s,e in edges:
+        if tree[s][0] == -1:
+            tree[s][0] = e
+        else:
+            tree[s][1] = e
+    wolf_sum = [0]*len(info)
+    wolf_sheep = [[] for _ in range(len(info))]
+    queue = deque([(0,[])])
+    while queue:
+        node, wolfs = queue.popleft()
+        for i in range(2):
+            if tree[node][i] != -1:
+                nxt = tree[node][i]
+                if info[nxt] == 1:
+                    nxt_wolfs = wolfs.copy()
+                    nxt_wolfs.append(nxt)
+                    queue.append((nxt, nxt_wolfs))
+                else:
+                    wolf_sum[nxt] = len(wolfs)
+                    queue.append((nxt, wolfs))
+                    wolf_sheep[nxt] = wolfs.copy()
+                    for wolf in wolfs:
+                        wolf_sheep[wolf].append(nxt)
+    answer = 0
+    def dfs(can, ans, wolf_num):
+        result = ans
+        for c in can:
+            if info[c] and ans <= wolf_num+1:
+                continue
+            nxt_can = can.copy()
+            del nxt_can[c]
+            for i in range(2):
+                if tree[c][i] != -1:
+                    nxt_can[tree[c][i]] = True
+            if info[c]:
+                result = max(result, dfs(nxt_can, ans, wolf_num+1))
+            else:
+                result = max(result, dfs(nxt_can, ans+1, wolf_num))
+        return result
+    answer = dfs({0: True}, 0, 0)
+    
+    return answer
