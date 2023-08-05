410866a4df4c1f20e6b979b3f786f31ab3ca87d4
zerohoon0102 <wodlxosxos73@gmail.com>
2023-08-05 17:42:04 +0900

    LeetCode : longest-increasing-subsequence v2( binary search )

--- leetcode/longest-increasing-subsequence.py
+++ leetcode/longest-increasing-subsequence.py
@@ -4,14 +4,35 @@ class Solution:
         result = 0
         
         for i, num in enumerate(nums):
-            cur_length = 1
-            for j in range(result, 0, -1):
-                if dp[j] < num:
-                    cur_length = j + 1
-                    break
+            cur_length = self.getNum(dp, num, 1, result)
             if cur_length in dp:
                 dp[cur_length] = min(num, dp[cur_length])
             else:
                 dp[cur_length] = num
-            result = max(result, cur_length)
+                result = cur_length
+        return result
+    
+    def getNum(self, dp, num, s, e):
+        mid = (s+e)//2
+        result = 1
+        while s < e:
+            if dp[mid] < num:
+                s = mid+1
+            elif dp[mid] > num:
+                e = mid
+            else:
+                break
+            mid = (s+e)//2
+        
+        if mid-1 in dp:
+            if dp[mid-1] < num <= dp[mid]:
+                result = mid
+            else:
+                result = mid+1
+        elif mid in dp:
+            if dp[mid] < num:
+                result = mid + 1
+            else:
+                result = mid
+        
         return result
