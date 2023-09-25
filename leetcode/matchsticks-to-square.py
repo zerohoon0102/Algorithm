ee3df0d3b3fc7aa0d1cc5316588cdfc6b6901e0b
Yun Young Hoon <wodlxosxos73@gmail.com>
2023-09-26 01:38:55 +0900

    LeetCode : matchsticks-to-square

---
+++ leetcode/matchsticks-to-square.py
@@ -0,0 +1,34 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        matchsticks.sort(reverse=True)
+        total = sum(matchsticks)
+        if total%4 > 0:
+            return False
+
+        one_side = total//4
+        length = len(matchsticks)
+        used = [False]*len(matchsticks)
+
+        def dfs(idx, cnt, cur):
+            result = False
+            if cnt == 4:
+                return True
+            for i in range(idx, length):
+                if used[i]:
+                    continue
+                nxt_v = cur+matchsticks[i]
+                if nxt_v > one_side:
+                    continue
+                else:
+                    used[i] = True
+                    if nxt_v == one_side:
+                        result = dfs(0, cnt+1, 0)
+                    else:
+                        result = dfs(i+1, cnt, nxt_v)
+                    if result:
+                        return True
+                    used[i] = False
+            return result
+
+        return dfs(0, 0, 0)
+    
